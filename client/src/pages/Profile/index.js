7561f04319f7925081abb9e12269d442016326a9
WrittenInnaCode <writteninnacode@gmail.com>
2023-07-20 20:26:46 -0700

    fixed a profile routing bug

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useEffect } from 'react';
+// import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
+import DiveList from '../../components/DiveList';
+
 import { QUERY_USER, QUERY_ME } from '../../utils/queries';
 
-import { Modal, Tab, Tabs } from 'react-bootstrap';
-import Login from '../../components/Login';
-import Signup from '../../components/Signup';
+// import { Modal, Tab, Tabs } from 'react-bootstrap';
+// import Login from '../../components/Login';
+// import Signup from '../../components/Signup';
 
 import Auth from '../../utils/auth';
 
@@ -22,83 +24,81 @@ const Profile = () => {
   const user = data?.me || data?.user || {};
 
   const navigate = useNavigate();
-  //is this working????????
 
-// Modal 
-  const [show, setShow] = useState(true);
-  const handleClose = () => setShow(false);
+  // Modal 
+  // const [show, setShow] = useState(true);
+  // const handleClose = () => setShow(false);
 
-
+useEffect(() => {
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return  navigate("/me");
+    return navigate("/me");
   }
+});
+  
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  if (!user?.username) {
-    return (
-      <div>
-        <Modal 
-        show={show} 
-        onHide={handleClose} 
-        backdrop="static"
-        keyboard={false}
-        centered>
-          <Modal.Header>
-            <Modal.Title>You need to be logged in to see this.</Modal.Title>
-          </Modal.Header>
+  // if (!user?.username) {
+  //   return (
+  //     <div>
+  //       <Modal 
+  //       show={show} 
+  //       onHide={handleClose} 
+  //       backdrop="static"
+  //       keyboard={false}
+  //       centered>
+  //         <Modal.Header>
+  //           <Modal.Title>You need to be logged in to see this.</Modal.Title>
+  //         </Modal.Header>
 
-          <Modal.Body className="bg-light">
-            <Tabs
-              defaultActiveKey="login"
-              className="mb-3, modalTabs"
-              fill
-              justify
-              id="modalTabs"
-            >
+  //         <Modal.Body className="bg-light">
+  //           <Tabs
+  //             defaultActiveKey="login"
+  //             className="mb-3, modalTabs"
+  //             fill
+  //             justify
+  //             id="modalTabs"
+  //           >
 
-              <Tab eventKey="login" title="Log In">
-                <Login />
-              </Tab>
+  //             <Tab eventKey="login" title="Log In">
+  //               <Login />
+  //             </Tab>
 
-              <Tab eventKey="signup" title="Sign Up">
-                <Signup />
-              </Tab>
+  //             <Tab eventKey="signup" title="Sign Up">
+  //               <Signup />
+  //             </Tab>
 
-            </Tabs>
+  //           </Tabs>
 
-          </Modal.Body>
-          <Modal.Footer>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  }
+  //         </Modal.Body>
+  //         <Modal.Footer>
+  //         </Modal.Footer>
+  //       </Modal>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div>
-      <h1>
+
+      {/* <h2>
         Welcome, {Auth.getProfile().data.username}!
-      </h1>
+      </h2> */}
 
-      <div>
-        <aside>
-          <nav>
-            <ul>
-              <li>
+      <h2>
+        Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+      </h2>
 
-              </li>
-              <li>
 
-              </li>
-              <li>
-
-              </li>
-            </ul>
-          </nav>
-        </aside>
+      <div className="col-12 col-md-10 mb-5">
+        <DiveList
+          dives={user.dives}
+          title={`${user.username}'s dives...`}
+          showTitle={false}
+          showUsername={false}
+        />
       </div>
 
     </div>
